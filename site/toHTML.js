001caee4761b5800c41012dda19704d36673639f
Brent Jackson <jxnblk@gmail.com>
2017-09-11 22:12:52 -0400

    Add GA

--- site/toHTML.js
+++ site/toHTML.js
@@ -10,4 +10,18 @@ body{font-family:-apple-system,BlinkMacSystemFont,sans-serif;line-height:1.25;ma
 </style>
 <style>${css}</style>
 ${body}
+<script>
+(function(i, s, o, g, r, a, m) {
+i["GoogleAnalyticsObject"] = r;
+i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+}, i[r].l = 1 * new Date();
+a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+a.async = 1;
+a.src = g;
+m.parentNode.insertBefore(a, m)
+})(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
+ga("create", "UA-78113757-1", "auto");
+ga("send", "pageview");
+</script>
 `
